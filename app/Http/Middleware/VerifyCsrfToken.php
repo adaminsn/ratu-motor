bcbc1a594c5db8e97049a14604f5cad5f9e8d6b7
adaminsaan <adaminsaan24@gmail.com>
2026-07-08 05:45:05 +0700

    Fix: Disable CSRF for all auth routes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'api/*',
+        'auth/*',
+        'login',
+        'register',
+        'logout',
     ];
 }
