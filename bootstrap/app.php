3b0fb3250b1245978c303b3e43ff9402cc56d9cf
adaminsaan <adaminsaan24@gmail.com>
2026-07-08 06:28:42 +0700

    eror

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Since you are using Bearer tokens (localStorage), you don't need stateful SPA authentication.
-        // This prevents Laravel from expecting CSRF tokens on API requests.
-        // $middleware->api(prepend: [
-        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        // ]);
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
         
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
