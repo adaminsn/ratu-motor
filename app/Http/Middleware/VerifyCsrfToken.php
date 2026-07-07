64e78693e74db73c0a19af0fe695f62d87bc6fbe
adaminsaan <adaminsaan24@gmail.com>
2026-07-08 06:22:38 +0700

    fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,10 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'api/*',
-        'auth/*',
-        'login',
-        'register',
-        'logout',
+        '*', 
     ];
 }
